27f1339a86be55ab98047931a9cb4cd6c484601d
jordankaplan <j4kaplan@student.bridgew.edu>
2020-09-23 15:03:41 -0400

    final house

--- arcade.demo.py
+++ arcade.demo.py
@@ -8,14 +8,26 @@ def main ():
     #now create objects
     main_house = arcade.create_rectangle(400, 200, 400, 400, arcade.color.ANTIQUE_BRASS)
     door= arcade.create_rectangle(400,75,100,150,arcade.color.ARMY_GREEN)
-    window_1= arcade.create_ellipse(300,300,75,75,arcade.color.AERO_BLUE)
+    window_1= arcade.create_ellipse(300,300,50,50,arcade.color.AERO_BLUE)
+    window_2 = arcade.create_ellipse(500,300,50,50,arcade.color.AERO_BLUE)
+    roof_points = [(200,400),(400,600),(600,400)]
+    roof= arcade.create_polygon(roof_points, arcade.color.DARK_GRAY)
+    line= arcade.create_line(200,730,510,730,arcade.color.ROSE)
+
     #now we will begin to draw
     arcade.start_render()
     #draw everything here
+    arcade.draw_text("boy is that an ugly house" , 200, 750, arcade.color.ALABAMA_CRIMSON, 22)
 
     main_house.draw()
     door.draw()
     window_1.draw()
+    window_2.draw()
+    roof.draw()
+    line.draw()
+
+
+
 
 
 
